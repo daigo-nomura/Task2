a0d497075e9638bdae92473e998d51945d80aa1a
daigo <daigon.0114@gmail.com>
2021-02-20 19:35:48 +0900

    swiftlintの確認

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by 野村大悟 on 2021/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
