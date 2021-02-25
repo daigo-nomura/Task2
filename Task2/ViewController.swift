416a546e81215f0d864a3df1b9b232a325992c9b
daigo <daigon.0114@gmail.com>
2021-02-25 22:09:03 +0900

    コード修正 + ファイル追加

--- Task2/ViewController.swift
+++ Task2/ViewController.swift
@@ -12,35 +12,28 @@ class ViewController: UIViewController {
     @IBOutlet private var topTextfield: UITextField!
     @IBOutlet private var bottomTextfield: UITextField!
     @IBOutlet private var resultLabel: UILabel!
-    private var flag: Int = 0
     @IBOutlet private var calculationSegment: UISegmentedControl!
-    private var resultNumber = 0.0
-
-    @IBAction private func calculationChange(_ sender: Any) {
-        flag =  calculationSegment.selectedSegmentIndex
-    }
 
     @IBAction private func calculationButton(_ sender: Any) {
         guard let topText = topTextfield.text else { return }
         guard let bottomText = bottomTextfield.text else { return }
         let topNumber = Double(topText) ?? 0
         let bottomNumber = Double(bottomText) ?? 0
-        switch flag {
+        switch calculationSegment.selectedSegmentIndex {
         case 0:
-            resultNumber = topNumber + bottomNumber
+            resultLabel.text = String(topNumber + bottomNumber)
         case 1:
-            resultNumber = topNumber - bottomNumber
+            resultLabel.text = String(topNumber - bottomNumber)
         case 2:
-            resultNumber = topNumber * bottomNumber
+            resultLabel.text = String(topNumber * bottomNumber)
         case 3:
-            if topNumber == 0.0 {
+            if bottomNumber == 0.0 {
                 resultLabel.text = "割る数には０以外を入力して下さい"
             } else {
-                resultNumber = topNumber / bottomNumber
+                resultLabel.text = String(topNumber / bottomNumber)
             }
         default:
-            break
+            return
         }
-        resultLabel.text = String(resultNumber)
     }
 }
